aadc5f4a6af1d47ddc450f0abc9e962a9079ef7a
nieves-mon <cunanan.nm@gmail.com>
2022-11-09 16:53:18 +0800

    Fix confirmable migration file

--- db/migrate/20221108104527_add_confirmable_to_devise.rb
+++ db/migrate/20221108104527_add_confirmable_to_devise.rb
@@ -4,7 +4,6 @@ class AddConfirmableToDevise < ActiveRecord::Migration[7.0]
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
     add_index :users, :confirmation_token, unique: true
-    User.update_all confirmed_at: DateTime.now
   end
 
   def down
