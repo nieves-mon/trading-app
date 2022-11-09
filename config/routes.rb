dc0291b9e5a28d0bcc5506a26ee96ec40ce08ee9
tammytamayao <tammytamayao@gmail.com>
2022-11-09 18:25:11 +0800

    fixed-logout-devise-session

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
   root 'pages#home'
-  
-    resources :admin
+  resources :admin
 end
