0f21327d8d8c3ab287c697599e665bc885bb7c1d
nieves-mon <cunanan.nm@gmail.com>
2022-11-10 16:18:34 +0800

    Create stocks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_09_120516) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_10_081157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stocks", id: false, force: :cascade do |t|
+    t.string "symbol"
+    t.string "name"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_09_120516) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "unconfirmed_email"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
