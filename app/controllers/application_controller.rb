86326577c5c47a81d1727ddefcfe3db5f17e7d9a
nieves-mon <cunanan.nm@gmail.com>
2022-11-12 12:11:01 +0800

    Add initialize_iex_client private method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
 class ApplicationController < ActionController::Base
-
     private
-    
+
     def admin_acess
         if current_user.admin
-            redirect_to accounts_path 
+            redirect_to accounts_path
         else
-            redirect_to traders_dashboard_path 
+            redirect_to traders_dashboard_path
         end
     end
-    
+
+    def initialize_iex_client
+        @client = IEX::Api::Client.new
+    end
 end
