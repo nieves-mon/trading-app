b7592c24facab5255e9ec2cf8e0f9f0e7c13c812
nieves-mon <cunanan.nm@gmail.com>
2022-11-14 23:59:11 +0800

    Modify search method and add show method

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,16 +1,30 @@
 class StocksController < ApplicationController
     before_action :initialize_iex_client
 
-    def new
+    def show
+        @stock = Stock.find_by(symbol: params[:symbol])
     end
 
     def search
         begin
 	        @quote = @client.quote(params[:symbol])
+
+            @stock = Stock.find_by(symbol: params[:symbol])
+
+            if @stock.nil?
+                @stock = Stock.new
+                @stock.symbol = @quote.symbol
+                @stock.name = @quote.company_name
+                @stock.price = @quote.latest_price
+                @stock.save
+            else
+                @stock.update_attribute(:price, @quote.latest_price)
+            end
+
+            redirect_to stock_path(@stock.symbol)
         rescue IEX::Errors::SymbolNotFoundError
-            @error = "Symbol not found. Please input a valid symbol."
-        rescue
-            @error = "Something went wrong. Please try again."
+            flash[:danger] = "Symbol not found. Please input a valid symbol."
+            render :new
         end
     end
 end
