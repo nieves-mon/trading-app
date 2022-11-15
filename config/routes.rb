bb02b514df8607011ec7a204f42dce29fdecf427
tammytamayao <tammytamayao@gmail.com>
2022-11-15 19:11:17 +0800

    rearrange code block

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 
@@ -12,10 +11,9 @@ Rails.application.routes.draw do
 
   scope "admin" do
     get "dashboard" => "admin#dashboard", as: :admin
-    resources :accounts
     get "pending" => "accounts#pending", as: :pending_account
     patch "accounts/:id/approve" => "accounts#approve", as: :approve_account
-
+    resources :accounts
   end
 
   scope "traders" do
