26db7decd800101dfe04e30fda9708d32586a79e
nieves-mon <cunanan.nm@gmail.com>
2022-11-16 18:48:35 +0800

    Change price display to call API instead of using db

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,4 +4,14 @@ class Stock < ApplicationRecord
     has_many :user_stocks
 
     validates :symbol, :name, :price, presence: true
+
+    def quote
+        begin
+            client = IEX::Api::Client.new
+            return client.quote(self.symbol)
+        rescue IEX::Errors::SymbolNotFoundError
+            flash[:danger] = "Symbol not found. Please input a valid symbol."
+            render :new
+        end
+    end
 end
