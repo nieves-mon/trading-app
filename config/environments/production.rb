afda7cc95fb90fb473750d38b93f2541aedecf0e
nieves-mon <cunanan.nm@gmail.com>
2022-11-16 17:29:17 +0800

    Configure default_url in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "trading_app_production"
+  config.action_mailer.default_url_options = { host: 'tradex.up.railway.app', protocol: 'https' }
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
