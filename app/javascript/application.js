9ed21b714abd4d026775bae03227b0c186815027
nieves-mon <cunanan.nm@gmail.com>
2022-11-22 18:41:38 +0800

    Remove bootstrap dependencies

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require jquery3
-//= require popper
-//= require bootstrap-sprockets
 
 import "@hotwired/turbo-rails"
 import "controllers"
