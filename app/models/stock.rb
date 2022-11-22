bf9a067c8be1d51abae0af8d9f5016f84be3b3b5
nieves-mon <cunanan.nm@gmail.com>
2022-11-22 19:18:54 +0800

    Remove unnecessary quote method

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,14 +4,4 @@ class Stock < ApplicationRecord
     has_many :user_stocks, dependent: :delete_all
 
     validates :symbol, :name, presence: true
-
-    def quote
-        begin
-            client = IEX::Api::Client.new
-            return client.quote(self.symbol)
-        rescue IEX::Errors::SymbolNotFoundError
-            flash[:danger] = "Symbol not found. Please input a valid symbol."
-            render :back
-        end
-    end
 end
