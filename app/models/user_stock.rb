7aa7d855bec420ccdbddaa7ee6b4c38dfd1dd058
nieves-mon <cunanan.nm@gmail.com>
2022-11-23 19:30:35 +0800

    Change validation of quantity column to accept zero

--- app/models/user_stock.rb
+++ app/models/user_stock.rb
@@ -2,5 +2,5 @@ class UserStock < ApplicationRecord
   belongs_to :user
   belongs_to :stock
 
-  validates :quantity, presence: true, numericality: {greater_than: 0, only_integer: true}
+  validates :quantity, presence: true, numericality: {greater_than_or_equal_to: 0, only_integer: true}
 end
