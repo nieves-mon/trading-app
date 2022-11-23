348a1b61574d75b054a84a1d042fd1060d59ad39
nieves-mon <cunanan.nm@gmail.com>
2022-11-23 19:59:41 +0800

    Refactor quantity column to have a default value of zero

--- app/models/user_stock.rb
+++ app/models/user_stock.rb
@@ -3,4 +3,5 @@ class UserStock < ApplicationRecord
   belongs_to :stock
 
   validates :quantity, presence: true, numericality: {greater_than_or_equal_to: 0, only_integer: true}
+  attribute :quantity, default: 0
 end
