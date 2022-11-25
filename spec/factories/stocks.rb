cf69bd77ebfddce91c94f13689a2dd4bb24ad5b5
nieves-mon <cunanan.nm@gmail.com>
2022-11-25 16:56:50 +0800

    Complete test specs for stock model

--- spec/factories/stocks.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :stock do
-    
-  end
-end
--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Stock, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when params are valid" do
+    it "is valid with valid attributes" do
+      expect(Stock.new(symbol: "MSFT", name: "Microsoft Corporation")).to be_valid
+    end
+  end
+
+  context "when params are invalid" do
+    it "is invalid without symbol" do
+      expect(Stock.new(symbol: nil, name: "Microsoft Corporation")).not_to be_valid
+    end
+    it "is invalid without name" do
+      expect(Stock.new(symbol: "MSFT", name: nil)).not_to be_valid
+    end
+  end
 end
