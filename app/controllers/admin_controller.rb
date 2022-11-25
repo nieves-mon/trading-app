12a3467caf383db38827be61c078bb70b9c1d0da
nieves-mon <cunanan.nm@gmail.com>
2022-11-25 22:17:05 +0800

    Move authorize_user before action to application controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,9 +1,8 @@
 class AdminController < ApplicationController
-    before_action :authenticate_user!
     before_action :authorize_admin
-    
+
     def transactions
         @transactions = Transaction.all
     end
-    
+
 end
