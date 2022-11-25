6781262ef6b127c35963e41c5cc58650f3092167
nieves-mon <cunanan.nm@gmail.com>
2022-11-25 15:31:04 +0800

    Add validation specs for transaction

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes"
+  it "is not valid without kind"
+  it "is not valid without quantity"
+  it "is not valid without price"
+  it "is not valid without amount"
+  it "is not valid without user"
+  it "is not valid without stock"
 end
