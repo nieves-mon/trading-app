8a05aa094cc761e15decbcda3f4e54bf0fa6419b
nieves-mon <cunanan.nm@gmail.com>
2022-11-25 16:31:07 +0800

    Complete test specs for transactions

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,11 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  it "is valid with valid attributes"
-  it "is not valid without kind"
-  it "is not valid without quantity"
-  it "is not valid without price"
-  it "is not valid without amount"
-  it "is not valid without user"
-  it "is not valid without stock"
+  user = FactoryBot.create(:user)
+  stock = FactoryBot.create(:stock)
+
+  context "when params are valid" do
+    it "is valid with valid attributes" do
+      expect(Transaction.new(kind: 0, quantity: 1, price: 89.9, amount: 89.9, stock: stock, user: user)).to be_valid
+    end
+  end
+
+  context "when params are invalid" do
+    it "is not valid when quantity is 0" do
+      expect(Transaction.new(kind: 0, quantity: 0, price: 89.9, amount: 89.9, stock: stock, user: user)).not_to be_valid
+    end
+    it "is not valid when quantity is less than 0" do
+      expect(Transaction.new(kind: 0, quantity: -1, price: 89.9, amount: 89.9, stock: stock, user: user)).not_to be_valid
+    end
+  end
+
+  context "when params are incomplete" do
+    it "is not valid without kind" do
+      expect(Transaction.new(kind: nil, quantity: 1, price: 89.9, amount: 89.9, stock: stock, user: user)).not_to be_valid
+    end
+    it "is not valid without quantity" do
+      expect(Transaction.new(kind: 0, quantity: nil, price: 89.9, amount: 89.9, stock: stock, user: user)).not_to be_valid
+    end
+    it "is not valid without price" do
+      expect(Transaction.new(kind: 0, quantity: 1, price: nil, amount: 89.9, stock: stock, user: user)).not_to be_valid
+    end
+    it "is not valid without amount" do
+      expect(Transaction.new(kind: 0, quantity: 1, price: 89.9, amount: nil, stock: stock, user: user)).not_to be_valid
+    end
+    it "is not valid without user" do
+      expect(Transaction.new(kind: 0, quantity: 1, price: 89.9, amount: 89.9, stock: stock, user: nil)).not_to be_valid
+    end
+    it "is not valid without stock" do
+      expect(Transaction.new(kind: 0, quantity: 1, price: 89.9, amount: 89.9, stock: nil, user: user)).not_to be_valid
+    end
+  end
 end
