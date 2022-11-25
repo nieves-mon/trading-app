6fee85914414e8bcaba9ed713d9c7460f14e2b0f
nieves-mon <cunanan.nm@gmail.com>
2022-11-25 19:52:34 +0800

    Refactor tests to utilize FactoryBot syntax

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,45 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  user = FactoryBot.create(:user)
-  stock = FactoryBot.create(:stock)
 
   context "when params are valid" do
-    it "is valid with valid attributes" do
-      expect(Transaction.new(kind: 0, quantity: 1, price: 89.9, amount: 89.9, stock: stock, user: user)).to be_valid
+    it "is valid with valid attributes as buy-transaction" do
+      expect(build(:transaction, :buy)).to be_valid
+    end
+    it "is valid with valid attributes as sell-transaction" do
+      expect(build(:transaction, :sell)).to be_valid
     end
   end
 
   context "when params are invalid" do
     it "is not valid if quantity is 0" do
-      expect(Transaction.new(kind: 0, quantity: 0, price: 89.9, amount: 89.9, stock: stock, user: user)).not_to be_valid
+      expect(build(:transaction, :buy, quantity: 0)).not_to be_valid
     end
     it "is not valid if quantity is less than 0" do
-      expect(Transaction.new(kind: 0, quantity: -1, price: 89.9, amount: 89.9, stock: stock, user: user)).not_to be_valid
+      expect(build(:transaction, :buy, quantity: -1)).not_to be_valid
     end
     it "is not valid if quantity is not an integer" do
-      expect(Transaction.new(kind: 0, quantity: 1.2, price: 89.9, amount: 107.88, stock: stock, user: user)).not_to be_valid
+      expect(build(:transaction, :buy, quantity: 1.2)).not_to be_valid
     end
   end
 
   context "when params are incomplete" do
     it "is not valid without kind" do
-      expect(Transaction.new(quantity: 1, price: 89.9, amount: 89.9, stock: stock, user: user)).not_to be_valid
+      expect(build(:transaction, kind: nil)).not_to be_valid
     end
     it "is not valid without quantity" do
-      expect(Transaction.new(kind: 0, price: 89.9, amount: 89.9, stock: stock, user: user)).not_to be_valid
+      expect(build(:transaction, kind: 0, quantity: nil)).not_to be_valid
     end
     it "is not valid without price" do
-      expect(Transaction.new(kind: 0, quantity: 1, amount: 89.9, stock: stock, user: user)).not_to be_valid
+      expect(build(:transaction, kind: 0, price: nil)).not_to be_valid
     end
     it "is not valid without amount" do
-      expect(Transaction.new(kind: 0, quantity: 1, price: 89.9, stock: stock, user: user)).not_to be_valid
+      expect(build(:transaction, kind: 0, amount: nil)).not_to be_valid
     end
     it "is not valid without user" do
-      expect(Transaction.new(kind: 0, quantity: 1, price: 89.9, amount: 89.9, stock: stock)).not_to be_valid
+      expect(build(:transaction, kind: 0, user: nil)).not_to be_valid
     end
     it "is not valid without stock" do
-      expect(Transaction.new(kind: 0, quantity: 1, price: 89.9, amount: 89.9, user: user)).not_to be_valid
+      expect(build(:transaction, kind: 0, stock: nil)).not_to be_valid
     end
   end
 end
