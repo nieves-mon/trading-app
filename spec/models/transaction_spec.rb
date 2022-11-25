8ce85009f1c939ac90a852e4627917d0b0a5547d
nieves-mon <cunanan.nm@gmail.com>
2022-11-25 16:51:49 +0800

    Add spec test for when quantity is not an integer

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -11,11 +11,14 @@ RSpec.describe Transaction, type: :model do
   end
 
   context "when params are invalid" do
-    it "is not valid when quantity is 0" do
+    it "is not valid if quantity is 0" do
       expect(Transaction.new(kind: 0, quantity: 0, price: 89.9, amount: 89.9, stock: stock, user: user)).not_to be_valid
     end
-    it "is not valid when quantity is less than 0" do
+    it "is not valid if quantity is less than 0" do
       expect(Transaction.new(kind: 0, quantity: -1, price: 89.9, amount: 89.9, stock: stock, user: user)).not_to be_valid
+    end
+    it "is not valid if quantity is not an integer" do
+      expect(Transaction.new(kind: 0, quantity: 1.2, price: 89.9, amount: 107.88, stock: stock, user: user)).not_to be_valid
     end
   end
 
