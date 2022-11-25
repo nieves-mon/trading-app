87f323563082eabad8de694660a34c6de4d31a91
nieves-mon <cunanan.nm@gmail.com>
2022-11-25 16:40:49 +0800

    Complete spec tests for user_stock model

--- spec/models/user_stock_spec.rb
+++ spec/models/user_stock_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe UserStock, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  user = FactoryBot.create(:user)
+  stock = FactoryBot.create(:stock)
+
+  context "when params are valid" do
+    it "is valid with complete attribute and quantity is 0" do
+      expect(UserStock.new(user: user, stock: stock, quantity: 0)).to be_valid
+    end
+    it "is valid with complete attribute and quantity is more than 0" do
+      expect(UserStock.new(user: user, stock: stock, quantity: 4)).to be_valid
+    end
+  end
+
+  context "when params are invalid" do
+    it "is not valid if quantity is less than 0" do
+      expect(UserStock.new(user: user, stock: stock, quantity: -1)).not_to be_valid
+    end
+  end
+
+  context "when params are incomplete" do
+    it "is not valid without user" do
+      expect(UserStock.new(user: nil, stock: stock, quantity: 1)).not_to be_valid
+    end
+    it "is not valid without stock" do
+      expect(UserStock.new(user: user, stock: nil, quantity: 1)).not_to be_valid
+    end
+    it "is not valid without quantity" do
+      expect(UserStock.new(user: user, stock: stock, quantity: nil)).not_to be_valid
+    end
+  end
 end
